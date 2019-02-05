5d13954c62b2aeb29f882f1d42012d7cfb0637d3
baris bilgic <brsbilgic@gmail.com>
2019-02-05 12:53:34 +0000

    Fixed unquoted

--- firebase_dynamic_links/builder.py
+++ firebase_dynamic_links/builder.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlencode
+from urllib.parse import urlencode, unquote
 
 from .client import FirebaseClient
 
@@ -9,8 +9,11 @@ def generate_short_link(client, app_code, query_params):
     return client.shorten_link(long_link=long_dynamic_link)
 
 
-def generate_long_link(app_code, query_params):
+def generate_long_link(app_code, query_params, unquoted=False):
     query_string = urlencode(query_params)
+    if unquoted:
+        query_string = unquote(query_string)
+
     link = 'https://{app_code}.page.link/?{query_string}'.format(app_code=app_code, query_string=query_string)
 
     return link
